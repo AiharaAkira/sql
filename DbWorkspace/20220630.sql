f22f7523b78ccb0cc82b6194fe9709360f304936
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2022-07-02 19:41:26 +0900

    CRUD

--- DbWorkspace/20220630.sql
+++ DbWorkspace/20220630.sql
@@ -11,3 +11,30 @@ create table department(
 commit;
 
 select * from department;
+
+
+create table professor(
+
+profno number(5) primary key,
+name varchar2(10) not null,
+ename varchar2(20) not null,
+position varchar2(20) not null,
+sal number(4) not null,
+hiredate date not null,
+age number(3) not null,
+deptno number(3) not null
+
+);
+
+
+--professor_fk
+
+ALTER TABLE professor
+ADD CONSTRAINT fk_department
+foreign KEY(deptno) references department(deptno);
+
+
+commit;
+insert into professor values(01,'test','test','test',1,sysdate,0,(select deptno from department where loc='7È£°ü'));
+
+SELECT * FROM professor;
